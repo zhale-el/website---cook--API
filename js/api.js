86f029848c0236a7d42e185387dd24aa968e36b8
zhaleh <zhalehelmi@gmail.com>
2023-09-11 20:31:17 +0330

    api

---
+++ js/api.js
@@ -0,0 +1,10 @@
+"use strict";
+
+window.ACCESS_POINT = "https://api.edamam.com/api/recipes/v2";
+const /**{String} */ APP_ID = "a9907020";
+const /**{String} */ APP_KEY = "31b21197eecd1096b9d3ee327757567c";
+const /**{String} */ TYPE = "public";
+
+export const fetchData = async function (queries, successCallback) {
+  const /**{String} */ query = queries?.join();
+};
