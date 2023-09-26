7f8da12cc584f36b52d1951739b00a4775091957
zhaleh <zhalehelmi@gmail.com>
2023-09-26 19:32:54 +0330

    start js detail

--- js/detail.js
+++ js/detail.js
@@ -1,1 +1,24 @@
 "use strict";
+
+/**
+ * Import
+ */
+
+import { fetchData } from "./api.js";
+import { getTime } from "./module.js";
+
+/**
+ * Render data
+ */
+
+const /**{NodeElement} */ $detailContainer = document.querySelector(
+    "[data-detail-container]"
+  );
+
+ACCESS_POINT += `/${window.location.search.slice(
+  window.location.search.indexOf("=") + 1
+)}`;
+
+// fetchData(null, (data) => {
+//   console.log(data);
+// });
